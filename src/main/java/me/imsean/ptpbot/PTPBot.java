1a03bafa9c6e1a9f05f2172c5ce74da57138257d
Dylan Curzon <d.46.curzon@hotmail.co.uk>
2015-10-13 18:18:31 +0100

    Move entry-point to Main class, implement more modular design to PTPBot class

--- src/main/java/me/imsean/ptpbot/PTPBot.java
+++ src/main/java/me/imsean/ptpbot/PTPBot.java
@@ -2,52 +2,65 @@ package me.imsean.ptpbot;
 
 import me.imsean.ptpbot.listeners.*;
 import xyz.gghost.jskype.SkypeAPI;
-import xyz.gghost.jskype.exception.SkypeException;
+import xyz.gghost.jskype.event.EventManager;
 
-import java.util.Scanner;
-
-/**
- * Created by sean on 10/11/15.
- */
 public class PTPBot {
 
-    static SkypeAPI skype;
-    static boolean isRunning = true;
-    static String OWNER = "master_zombiecow";
+    // TODO: Have these passed via Dependency Injection.
+    private static final String OWNER = "master_zombiecow";
+    private static SkypeAPI skype;
 
-    private String username, password;
+    private boolean running;
 
-
-    public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter Credentials (format: user:pass)");
-        String[] creds = in.nextLine().split(":");
-        skype = new SkypeAPI(creds[0], creds[1]);
-        skype.getEventManager().registerListener(new CaptchaListener());
-        try {
-            skype.login();
-            System.out.println("PTPBot running!");
-        } catch (SkypeException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        skype.getEventManager().registerListener(new ContactRequestListener());
-        skype.getEventManager().registerListener(new CommandListener());
-        skype.getEventManager().registerListener(new ChatListener());
-        skype.getEventManager().registerListener(new BannedUserJoinListener());
-        skype.getEventManager().registerListener(new StatsMessageCountListener());
-        skype.getEventManager().registerListener(new UnpermittedTopicChangeListener());
-        skype.getEventManager().registerListener(new UnpermittedTopicPictureChangeListener());
-
-        while(isRunning) {}
-        skype.stop();
+    public static String getOwner() {
+        return OWNER;
     }
 
     public static SkypeAPI getSkype() {
         return skype;
     }
-    public static String getOwner() { return OWNER; }
+
+    public void login(String username, String password) {
+        if (this.running) {
+            stop();
+        }
+
+        try {
+            skype = new SkypeAPI(username, password);
+            skype.login();
+
+            System.out.println("Successfully logged in!");
+        } catch (Exception e) {
+            throw new RuntimeException("An exception occurred whilst logging in!", e);
+        }
+    }
+
+    public void start() {
+        this.running = true;
+        registerListeners();
+
+        System.out.println("PTPBot has started!");
+    }
+
+    public void stop() {
+        this.running = false;
+        skype.stop();
+        skype = null;
+    }
+
+    private void registerListeners() {
+        if (skype == null) {
+            return;
+        }
+
+        final EventManager em = skype.getEventManager();
+        em.registerListener(new ContactRequestListener());
+        em.registerListener(new CommandListener());
+        em.registerListener(new ChatListener());
+        em.registerListener(new BannedUserJoinListener());
+        em.registerListener(new StatsMessageCountListener());
+        em.registerListener(new UnpermittedTopicChangeListener());
+        em.registerListener(new UnpermittedTopicPictureChangeListener());
+    }
 
 }
